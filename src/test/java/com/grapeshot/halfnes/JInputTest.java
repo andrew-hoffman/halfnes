bd9acf3e717297f3d8072e723d2cab854d81ccf8
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2015-11-29 23:31:46 +0100

    temporary disable launch4j packaging add simple JInput test case

---
+++ src/test/java/com/grapeshot/halfnes/JInputTest.java
@@ -0,0 +1,28 @@
+package com.grapeshot.halfnes;
+
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by KlausH on 29.11.2015.
+ */
+public class JInputTest {
+
+    static {
+        JInputHelper.setupJInput();
+    }
+
+    @Test
+    public void testJInput() {
+        ControllerEnvironment controllerEnvironment = ControllerEnvironment.getDefaultEnvironment();
+        Controller[] controllers = controllerEnvironment.getControllers();
+        System.out.println(String.format("%s controllers found.", controllers.length));
+        Arrays.asList(controllers).forEach(controller -> {
+            System.out.println(String.format("  %s", controller));
+        });
+    }
+
+}
